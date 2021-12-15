4098fe4438a913825e852a9bc42cd5938fd09858
Hyeongyu Lee <dlgusrb1913@naver.com>
2021-12-15 16:29:16 +0900

    update test

--- src/main/java/com/my/customer/customerSelvlet.java
+++ src/main/java/com/my/customer/customerSelvlet.java
@@ -18,14 +18,12 @@ public class customerSelvlet extends HttpServlet {
      * Default constructor. 
      */
     public customerSelvlet() {
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -33,7 +31,6 @@ public class customerSelvlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
