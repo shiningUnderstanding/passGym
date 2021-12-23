4f5c3460f638d69749577c4c3d750ed5f74cfec4
IIlIIllIIll <qt1223@naver.com>
2021-12-23 10:05:54 +0900

    DAOOracle, DAOInterface, gitignore
    DAOOracle, DAOInterface method 추가
    gitignore MyConnection -> PassGymConnection으로 변경

---
+++ src/main/java/com/passgym/zzim/dao/ZzimDAOOracle.java
@@ -0,0 +1,16 @@
+package com.passgym.zzim.dao;
+
+import com.passgym.exception.AddException;
+import com.passgym.exception.RemoveException;
+import com.passgym.zzim.vo.Zzim;
+
+public class ZzimDAOOracle implements ZzimDAOInterface {
+
+	public void addZzim(Zzim zzim) throws AddException{
+	
+	}
+	
+	public void removeZzim(Zzim zzim) throws RemoveException{
+		
+	}
+}
