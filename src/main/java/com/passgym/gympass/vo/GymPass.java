38739e6635dbdae24da17b4a0c791dc2e779da71
Hyeongyu Lee <dlgusrb1913@naver.com>
2021-12-23 20:43:29 +0900

    fix compile error

--- src/main/java/com/passgym/gympass/vo/GymPass.java
+++ src/main/java/com/passgym/gympass/vo/GymPass.java
@@ -27,5 +27,106 @@ public class GymPass {
 	private Star star;
 	
 	public GymPass() {}
-	
+
+	public GymPass(int paymentNo, Pass pass, User user, Date startDate, Date endDate, int pauseCount, int pauseDate,
+			int status, Payment payment, Star star) {
+		super();
+		this.paymentNo = paymentNo;
+		this.pass = pass;
+		this.user = user;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.pauseCount = pauseCount;
+		this.pauseDate = pauseDate;
+		this.status = status;
+		this.payment = payment;
+		this.star = star;
+	}
+
+	public int getPaymentNo() {
+		return paymentNo;
+	}
+
+	public void setPaymentNo(int paymentNo) {
+		this.paymentNo = paymentNo;
+	}
+
+	public Pass getPass() {
+		return pass;
+	}
+
+	public void setPass(Pass pass) {
+		this.pass = pass;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getPauseCount() {
+		return pauseCount;
+	}
+
+	public void setPauseCount(int pauseCount) {
+		this.pauseCount = pauseCount;
+	}
+
+	public int getPauseDate() {
+		return pauseDate;
+	}
+
+	public void setPauseDate(int pauseDate) {
+		this.pauseDate = pauseDate;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Star getStar() {
+		return star;
+	}
+
+	public void setStar(Star star) {
+		this.star = star;
+	}
+
+	@Override
+	public String toString() {
+		return "GymPass [paymentNo=" + paymentNo + ", pass=" + pass + ", user=" + user + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", pauseCount=" + pauseCount + ", pauseDate=" + pauseDate + ", status="
+				+ status + ", payment=" + payment + ", star=" + star + "]";
+	}
 }
