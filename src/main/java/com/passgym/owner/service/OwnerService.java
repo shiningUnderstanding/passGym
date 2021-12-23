3663a6acbd6ff53041ee387c9bb6960193481a93
Kim Hak Hyeon 95 <gkrgus0810@gmail.com>
2021-12-23 21:36:25 +0900

    Update OwnerService.java

--- src/main/java/com/passgym/owner/service/OwnerService.java
+++ src/main/java/com/passgym/owner/service/OwnerService.java
@@ -1,13 +1,13 @@
 package com.passgym.owner.service;
 
 
-public class OwnerService {
-	
-
 import com.passgym.exception.AddException;
 import com.passgym.gym.dao.GymDAOOracle.GymDAOOracle;
 
 public class OwnerService {
+	
+
+
 	public void findByOwnerNo(int ownerNo) throws AddException{
 	 
 	}
