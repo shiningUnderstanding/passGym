8587220b2f9274b9d84e6669d5dc736fe8cd31d4
Kim Hak Hyeon 95 <gkrgus0810@gmail.com>
2021-12-29 20:58:31 +0900

    Update PassDAOOracle.java

--- src/main/java/com/passgym/pass/dao/PassDAOOracle.java
+++ src/main/java/com/passgym/pass/dao/PassDAOOracle.java
@@ -58,5 +58,35 @@ public class PassDAOOracle implements PassDAOInterface {
 		return passes;
 	}
 
-
+	@Override
+	public void add(List<Pass> passes) throws AddException {
+		Pass pass = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String insertSQL = "INSERT INTO pass(owner_no, pass_no, pass_name, pass_price, pass_status, pass_month, pause_count, pause_date, remarks)\r\n"
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		for(int i = 0; i < passes.size(); i++) {
+			pass = passes.get(i);
+			try {
+				con = PassGymConnection.getConnection();
+				pstmt = con.prepareStatement(insertSQL);
+				pstmt.setInt(1, pass.getOwnerNo());
+				pstmt.setInt(2, pass.getPassNo());
+				pstmt.setString(3, pass.getPassName());
+				pstmt.setInt(4, pass.getPassPrice());
+				pstmt.setInt(5, pass.getPassStatus());
+				pstmt.setInt(6, pass.getPassMonth());
+				pstmt.setInt(7, pass.getPauseCount());
+				pstmt.setInt(8, pass.getPauseDate());
+				pstmt.setString(9, pass.getRemarks());
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally {
+				PassGymConnection.close(pstmt, con);
+			}
+		}
+	}
+	
 }
