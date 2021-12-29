8d5ee9ddebd6370ec0b74e84c31e5f60b2ddbbe9
Hyeongyu Lee <dlgusrb1913@naver.com>
2021-12-29 21:33:13 +0900

    fix error

--- src/main/webapp/js/menu.js
+++ src/main/webapp/js/menu.js
@@ -1,39 +1,36 @@
 /**
- * 
+ *
  */
+function menuClick() {
+  let $menuObj = $(".link");
+  $menuObj.click(function () {
+    let menuHref = $(this).attr("href");
+    let ajaxUrl = "";
+    let ajaxMethod = "";
+    switch (menuHref) {
+      case "login.jsp":
+        ajaxUrl = menuHref;
+        ajaxMethod = "get";
+        $("section").empty();
+        $("section").load(ajaxUrl, function (responseText, textStatus, jqXHR) {
+          if (jqXHR.status != 200) {
+            alert("응답실패:" + jqXHR.status);
+          }
+        });
+        return false;
 
-function menuClick(){
-    let $menuObj = $(".link");
-    $menuObj.click(function() {
-        let menuHref = $(this).attr("href");
-        let ajaxUrl = "";
-        let ajaxMethod = "";
-        switch (menuHref) {
-        case "login":
-            ajaxUrl = menuHref;
-            ajaxMethod = "get";
-            
-        case "mypage":
-            //AJAX요청, 응답
-            ajaxUrl = menuHref;
-            ajaxMethod = "post";
-            let userNo = 2;
-            $("section").empty();
-            //( String responseText, String textStatus, jqXHR jqXHR)
-            $.ajax({
-                url : ajaxUrl,
-                method : ajaxMethod,
-                data : {
-                    userNo : userNo
-                }, 
-                success : function(responseData) {
-                    $("section").html(responseData);
-                },
-                error : function(xhr) {
-                    alert("응답실패 status : " + xhr.status);
-                },
-            });
-            return false;
-        }
-    });
+      case "logout":
+        ajaxUrl = menuHref;
+        $.ajax({
+          url: ajaxUrl,
+          success: function () {
+            location.href = "./index.jsp";
+          },
+          error: function (xhr) {
+            alert("응답실패: " + xhr.status);
+          },
+        });
+        return false;
+    }
+  });
 }
